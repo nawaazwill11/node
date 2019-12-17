a7eaa3a46f681a00846d6073b52b4ae21cbfec3b
nawaazwill11 <mastermindjim@gmail.com>
2019-12-17 15:31:26 +0530

    added grid / list view capabilities to store

--- figa/js/store.js
+++ figa/js/store.js
@@ -16,6 +16,30 @@ const box_next = Array.from(document.getElementsByClassName('box-next'));
 box_next.forEach(next => {
     next.addEventListener('click', function () {
         this.parentNode.parentNode.parentNode.parentNode.children[2].style.display = 'block';
-        console.log(this.parentNode.parentNode.parentNode.parentNode.children[2]);
     });
 });
+
+const file_adder = document.getElementById('file-adder');
+file_adder.addEventListener('click', function () {
+    setTimeout(() => {
+        this.style.backgroundColor = 'aliceblue';    
+    }, 100);
+    this.style.backgroundColor = '#dce4ea';
+});
+
+const view = document.getElementById('view-dropdown');
+view.addEventListener('change', function () {
+    let row_box = document.getElementsByClassName('row-box')[0];
+    let box_main = document.getElementsByClassName('box-main')[0];
+    let box_menu = document.getElementsByClassName('box-menu')[0];
+    if (this.value == 'Grid') {
+        row_box.classList.remove('row-box-list');
+        box_main.classList.remove('box-main-list');
+        box_menu.classList.remove('box-menu-list');
+    }
+    else {
+        row_box.classList.add('row-box-list');
+        box_main.classList.add('box-main-list');
+        box_menu.classList.add('box-menu-list');
+    }
+})
